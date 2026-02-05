fbef92f163eff0df2d90c6440dc7be2ad038a446
khaatriinaa <khatrinagonzales4@gmail.com>
2026-02-05 18:19:18 +0800

    15th commit

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,3 +1,4 @@
+// src/navigation/AppNavigator.tsx
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -7,52 +8,44 @@ import Icon from "react-native-vector-icons/Ionicons";
 import HomeScreen from "../screens/HomeScreen";
 import CartScreen from "../screens/CartScreen";
 import CheckoutScreen from "../screens/CheckoutScreen";
+import { useCart } from "../contexts/CartContext";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-// Create a stack for Home tab
-const HomeStack = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerShown: false,
-      animation: "slide_from_right",
-    }}
-  >
-    <Stack.Screen name="HomeMain" component={HomeScreen} />
-    <Stack.Screen name="Checkout" component={CheckoutScreen} />
-  </Stack.Navigator>
-);
+const Tabs = () => {
+  const { count } = useCart();
 
-// Create a stack for Cart tab
-const CartStack = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerShown: false,
-      animation: "slide_from_right",
-    }}
-  >
-    <Stack.Screen name="CartMain" component={CartScreen} />
-    <Stack.Screen name="Checkout" component={CheckoutScreen} />
-  </Stack.Navigator>
-);
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused }) => {
+          let icon = "home";
+          if (route.name === "Cart") icon = "cart";
+          return <Icon name={icon} size={22} />;
+        },
+        tabBarBadge: route.name === "Cart" && count > 0 ? count : undefined,
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
+      <Stack.Navigator
+        screenOptions={{
           headerShown: false,
-          tabBarIcon: ({ focused }) => {
-            let icon = "home";
-            if (route.name === "Cart") icon = "cart";
-            return <Icon name={icon} size={22} />;
-          },
-        })}
+          animation: "slide_from_right",
+        }}
       >
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Cart" component={CartStack} />
-      </Tab.Navigator>
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Checkout" component={CheckoutScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
