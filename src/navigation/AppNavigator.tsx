1e9c81c68dbfad6c98e38c50ca68a1b1e9367451
khaatriinaa <khatrinagonzales4@gmail.com>
2026-02-05 20:08:20 +0800

    21st commit - Cart Badge

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,4 +1,3 @@
-// AppNavigator.tsx
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -7,13 +6,16 @@ import HomeScreen from "../screens/HomeScreen";
 import CartScreen from "../screens/CartScreen";
 import CheckoutScreen from "../screens/CheckoutScreen";
 import { useTheme } from "../contexts/ThemeContext";
+import { useCart } from "../contexts/CartContext";
 import Icon from "react-native-vector-icons/Ionicons";
+import { View, Text, StyleSheet } from "react-native";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   const { dark } = useTheme();
+  const { cart } = useCart(); // get cart length
 
   const TabScreens = () => (
     <Tab.Navigator
@@ -34,11 +36,17 @@ const AppNavigator = () => {
           tabBarIcon: ({ color, size }) => <Icon name="home" size={size} color={color} />,
         }}
       />
+
       <Tab.Screen
         name="Cart"
         component={CartScreen}
         options={{
           tabBarIcon: ({ color, size }) => <Icon name="cart" size={size} color={color} />,
+          tabBarBadge: cart.length > 0 ? cart.length : undefined, // show badge
+          tabBarBadgeStyle: {
+            backgroundColor: "#FF3B30", // red badge
+            color: "#fff",
+          },
         }}
       />
     </Tab.Navigator>
